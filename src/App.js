3b8006f7dac3d8668ef6bf865183f288219b538f
BLADIMIR ROJAS <bladimirrojassepulveda@gmail.com>
2022-02-15 19:02:22 -0500

    si

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          "Find your perfect trip, designed by insiders who know and love their
+          cities!".
         </p>
         <a
           className="App-link"
