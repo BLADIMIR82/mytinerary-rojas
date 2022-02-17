ce42cd9c46b1d4ae267791b7c97e88e7087fb2d0
BLADIMIR ROJAS <bladimirrojassepulveda@gmail.com>
2022-02-16 19:43:47 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ResponsiveAppBar from "./componentes/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {" "}
-          "Find your perfect trip, designed by insiders who know and love their
-          cities!".
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <ResponsiveAppBar />
+    </header>
   );
 }
 
